eab2a9e59d52d1b96eb0437079fed2b1df7c13a0
Josh Krause <josh@tride.ca>
2019-02-08 13:25:14 -0700

    Fix hot topics link
    Signed-off-by: Josh Krause <josh@tride.ca>

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,8 +20,7 @@
                         </a>
                         @endunless
                         <h2 class="title font-light"><a href="/communications/topics/{{$post->id}}" class="link">{{$post->title}}</a></h2>
-                        <p class="m-t-30 m-b-30">{!!str_limit($post->content, 100)!!}</p>
-                        <a href="/communications/topics/{{$post->id}}" class="font-13">CONTINUE READING</a>
+                        <p><a href="/communications/topics/{{$post->id}}" class="font-13">CONTINUE READING</a></p>
                     </div>
                     <!-- Blog  -->
                     <hr class="op-5" />
