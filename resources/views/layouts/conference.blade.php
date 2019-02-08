16055a2522f00cdb074d4253b0aba3ddc231dc2d
Josh Krause <josh@tride.ca>
2019-02-08 13:32:20 -0700

    Add conference details
    Signed-off-by: Josh Krause <josh@tride.ca>

--- resources/views/layouts/conference.blade.php
+++ resources/views/layouts/conference.blade.php
@@ -340,7 +340,10 @@
                         <li class=""><i class="far fa-calendar-alt"></i> <strong>Saturday</strong>  <span>- April 6 2019</span></li>
                      </ul>
                      <a href="{{asset('storage/conference/2019/2019-aapg-conference.pdf')}}" class="but mt-30 "  title="Download Program"> Download Program</a>
-                  </div>
+                     <a href="{{asset('storage/conference/2019/agm-call-for-nominiations.pdf')}}" class="but mt-30 "  title="Download Program"> Call For Nominations</a>
+                     <a href="{{asset('storage/conference/2019/agm-call-for-resolutions.pdf')}}" class="but mt-30 "  title="Download Program"> Call For Resolutions</a>
+                     <a href="{{asset('storage/conference/2019/rfp-host-conference.pdf')}}" class="but mt-30 "  title="Download Program"> RFP To Host Conference</a>
+                    </div>
                   <div class="col-sm-8 ">
                      <ul class="block-tab">
                         <li> <!-- Thursday -->
@@ -642,7 +645,7 @@
                      </h3>
                      <div class="review-slider flexslider">
                         <ul class="slides">
-                           
+
                            <li>
                               <blockquote>Buy 3 member tickets, and save $75 on the fourth ticket! Discount is applied automatically.</blockquote>
                            </li>
