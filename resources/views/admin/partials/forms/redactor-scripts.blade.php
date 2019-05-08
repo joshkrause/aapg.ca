ee34a44fa10a9e4c443afe6d8baf555c9abf4c78
Joshua Krause <1243836+joshkrause@users.noreply.github.com>
2019-05-08 14:12:31 -0600

    Update redactor-scripts.blade.php

--- resources/views/admin/partials/forms/redactor-scripts.blade.php
+++ resources/views/admin/partials/forms/redactor-scripts.blade.php
@@ -1,6 +1,6 @@
 <script>
     $R('.redactor-textarea', {
-        plugins: ['video', 'filemanager', 'widget', 'imagemanager'],
+        plugins: ['video', 'filemanager', 'widget', 'imagemanager', 'alignment'],
         imageUpload: '/admin/redactor/images',
         imageManagerJson: '/admin/imagemanager/',
         fileUpload: '/admin/redactor/files',
